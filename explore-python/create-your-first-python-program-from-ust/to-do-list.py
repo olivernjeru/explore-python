923273496df398b67ff7b6521eff2ec85180c685
Oliver Njeru <olivernjiru@gmail.com>
2023-09-10 12:56:50 +0300

    create user_input, tasks var & show_menu()

---
+++ explore-python/create-your-first-python-program-from-ust/to-do-list.py
@@ -0,0 +1,11 @@
+user_input = ''
+tasks = []
+
+def show_menu():
+    print('Welcome to My To-Do List!')
+    print('Enter only the digits below to access My To-Do List functionalities')
+    print('1. Add Tasks')
+    print('2. View Available Tasks')
+    print('3. Remove a Task')
+    print('4. Exit')
+
