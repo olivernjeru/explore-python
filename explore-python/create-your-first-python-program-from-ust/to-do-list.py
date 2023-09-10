0356da65d52972b95c64e92f3d677cb844848614
Oliver Njeru <olivernjiru@gmail.com>
2023-09-10 13:28:50 +0300

    create version1 of to-do list

--- explore-python/create-your-first-python-program-from-ust/to-do-list.py
+++ explore-python/create-your-first-python-program-from-ust/to-do-list.py
@@ -11,3 +11,26 @@ def show_menu():
 
 while user_input != '4':
     show_menu()
+    user_input = input('Enter your selection: ')
+    if user_input == '1':
+        item = input('Add a task: ')
+        tasks.append(item)
+        print(item, 'was added to your to-do list!')
+    elif user_input == '2':
+        if tasks != []:
+            print('These is your list of tasks: ')
+            for item in tasks:
+                print(item)
+        else:
+            print('You have no tasks yet!')
+    elif user_input == '3':
+        item = input('Please enter the task you want to remove: ')
+        if item in tasks:
+            tasks.remove(item)
+            print(item, 'has been removed from your to-do list!')
+        else:
+            print(item, 'could not be found in your to-do list')
+    elif user_input =='4':
+        print('Goodbye!')
+    else:
+        print('Please enter either 1, 2, 3 or 4.')
